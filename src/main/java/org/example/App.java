5f4808192828e99b506e1a53d12c023b00d18494
patrakhin <patrakhin@mail.ru>
2023-07-11 22:52:43 +0300

    cascade in hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -25,52 +25,12 @@ public class App
         try {
             session.beginTransaction();
 
-            /*Person_OtM personOtM = session.get(Person_OtM.class, 3);
-            System.out.println(personOtM);
+            Person_OtM personOtM = new Person_OtM("Test person", 30);
+            personOtM.addItem(new Item("Item 1"));
+            personOtM.addItem(new Item("Item 2"));
+            personOtM.addItem(new Item("Item 3"));
 
-            List<Item> items = personOtM.getItems();
-            System.out.println(items);*/
-
-            /*Item item = session.get(Item.class, 5);
-            System.out.println(item);
-
-            Person_OtM personOtM = item.getOwner();
-            System.out.println(personOtM);*/
-
-            /*Person_OtM personOtM = session.get(Person_OtM.class, 2);
-
-            Item newItem = new Item("Item for Hibernate", personOtM);
-
-            personOtM.getItems().add(newItem);
-
-            session.save(newItem);*/
-
-            /*Person_OtM personOtM = new Person_OtM("Andrey", 47);
-            Item newItem = new Item("automobile", personOtM);
-            personOtM.setItems(new ArrayList<>(Collections.singletonList(newItem)));
             session.save(personOtM);
-            session.save(newItem);*/
-
-            /*Person_OtM personOtM = session.get(Person_OtM.class, 3);
-            List<Item> items = personOtM.getItems();
-            for (Item item:items) {
-                session.remove(item);
-            }
-
-            personOtM.getItems().clear();*/
-
-            /*Person_OtM personOtM = session.get(Person_OtM.class, 2);
-            session.remove(personOtM);
-
-            personOtM.getItems().forEach(i -> i.setOwner(null));*/
-
-            Person_OtM personOtM = session.get(Person_OtM.class, 4);
-            Item item = session.get(Item.class, 1);
-            item.getOwner().getItems().remove(item);
-
-            item.setOwner(personOtM);
-            personOtM.getItems().add(item);
-
 
             session.getTransaction().commit();
         } finally {
--- src/main/java/org/example/model/Person_OtM.java
+++ src/main/java/org/example/model/Person_OtM.java
@@ -1,6 +1,9 @@
 package org.example.model;
 
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -50,6 +53,7 @@ public class Person_OtM {
     }
 
     @OneToMany(mappedBy = "owner")
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
     private List<Item> items;
 
     public List<Item> getItems() {
@@ -58,6 +62,14 @@ public class Person_OtM {
 
     public void setItems(List<Item> items) {
         this.items = items;
+    }
+
+    public void addItem(Item item){
+        if (this.items == null){
+            this.items = new ArrayList<>();
+        }
+        this.items.add(item);
+        item.setOwner(this);
     }
 
     @Override
